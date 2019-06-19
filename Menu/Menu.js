5c4eb6134a3db78f372dd824e4ef41003731713b
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-06-19 16:30:10 -0500

    added slide-out animation to menu panel

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,19 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
+  // menu.classList.toggle("menu--open");
+
+  // Animation to slide out menu panel
+  // ***** Animation to slide panel back not working
+  if (menu.classList.contains("menu--closed")) {
+    // menu.style.display = "block";
+    menu.style.animation = "menuSlideOut 1s";
+    menu.classList.toggle("menu--open");
+    menu.classList.toggle("menu--closed");
+  } else {
+    menu.classList.toggle("menu--open");
+    menu.classList.toggle("menu--closed");
+    // menu.style.animation = "menuSlideIn 1s";
+  }
 };
 
 // Start Here: Create a reference to the ".menu" class
