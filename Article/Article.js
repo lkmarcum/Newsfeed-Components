e3ee1be881d61962c6aae56ab06df5542037d420
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-07-10 14:23:27 -0500

    created object area for article data

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,22 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement;
+//     // create a reference to the ".expandButton" class.
+//     this.expandButton;
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
 
-  }
-}
+//   }
+
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+
+//   }
+// }
 
 /* START HERE: 
 
@@ -26,4 +26,46 @@ class Article {
 
 */
 
-let articles;
+// let articles;
+
+const articleData = [
+  {
+    title: `Lambda School Students: "We're the best!"`,
+    date: 'Nov 5th, 2017',
+    content: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+    moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth baba skywalker
+    watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau darth calamari.
+    Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine skywalker. Qui-gonn
+    jar twi'lek jinn leia jango skywalker mon.`
+  }
+  {
+    title: `Javascript and You, ES6`,
+    date: 'Nov 7th, 2017',
+    content: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
+    Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
+    snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
+    yew pumpkin juice phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia Pigwidgeon
+    knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed.
+    Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks.`
+  }
+  {
+    title: `React vs Angular vs Vue`,
+    date: 'Nov 7th, 2017',
+    content: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
+    adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit. Squirtle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wartortle Lorem ipsum dolor
+    sit amet, consectetur adipiscing elit. Blastoise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Caterpie Lorem
+    ipsum dolor sit amet, consectetur adipiscing elit. Metapod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Butterfree
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit.`
+  }
+  {
+    title: `Professional Software Development in 2018`,
+    date: 'Nov 7th, 2017',
+    content: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+    hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+    Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor?`
+  }
+]
